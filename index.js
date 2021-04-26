982659ba6b75e0012734f3e73f27aadead3b6cd9
Nicolas Romero <nicooromero@gmail.com>
2021-04-25 21:00:39 -0300

    port config

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(cors());
 
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/transactions', require('./routes/transactions'));
 
-app.listen(PORT, () => {
-    console.log(`servidor funcionando ${PORT}`)
+app.listen(port, '0.0.0.0', () => {
+    console.log(`servidor funcionando ${port}`)
 });
