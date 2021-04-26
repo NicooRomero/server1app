7d7b72812cd27e61d703a2d3e77276814836a4cc
Nicolas Romero <nicooromero@gmail.com>
2021-04-25 23:21:47 -0300

    cors check

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const app = express();
 
 connectDB();
 
-// const whitelist = ['https://lucid-volhard-2f67c5.netlify.app', 'https://radiant-citadel-25364.herokuapp.com/', 'http://localhost:3000','http://localhost:4000'];
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         const exist = whitelist.some( domain => domain === origin );
-//         if(exist) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('not allowed by cors'))
-//         }
-//     }
-// }
+const whitelist = ['https://lucid-volhard-2f67c5.netlify.app', 'https://radiant-citadel-25364.herokuapp.com/', 'http://localhost:3000','http://localhost:4000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        const exist = whitelist.some( domain => domain === origin );
+        if(exist) {
+            callback(null, true)
+        } else {
+            callback(new Error('not allowed by cors'))
+        }
+    }
+}
 
 
 app.use(cors());
-app.options('*', cors());
 
 app.use(express.json({ extended: true }));
 
